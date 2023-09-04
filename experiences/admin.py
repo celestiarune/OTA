afd90e8f808f2f547912a951dd997d6171068c53
celestiarune <celestiarune@gmail.com>
2023-09-04 15:43:21 +0900

    add categories app

--- experiences/admin.py
+++ experiences/admin.py
@@ -11,6 +11,10 @@ class ExperienceAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
+    
+    list_filter = (
+        "category",
+    )
 
 @admin.register(Perk)
 class PerkAdmin(admin.ModelAdmin):
--- rooms/models.py
+++ rooms/models.py
@@ -20,8 +20,10 @@ class Room(CommonModel):
     address = models.CharField(max_length=250)
     pet_friendly = models.BooleanField(default=True)
     kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
+    
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     amenities = models.ManyToManyField("rooms.Amenity")
+    category = models.ForeignKey("categories.Category", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self) -> str:
         return self.name
