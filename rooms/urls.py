01fa486a6a4a6f668a8286fd05a51b2d50ab0fd6
celestiarune <celestiarune@gmail.com>
2023-09-07 18:34:13 +0900

    delete templates

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,1 @@
 from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.see_all_rooms),
-    path("<int:room_id>", views.see_one_room),
-]
--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,2 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
 
-def see_all_rooms(request):
-    rooms = Room.objects.all()
-    return render(
-        request, 
-        "all_rooms.html", 
-        {
-            'rooms':rooms,
-            'title':"Hello this title is comes from Django!",
-        }
-    )
-
-def see_one_room(request, room_id):
-    return HttpResponse(f"see room with id: {room_id}")
