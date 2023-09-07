edf95f2f987ad3e1c5ff319710cdb90b6ef73152
celestiarune <celestiarune@gmail.com>
2023-09-08 01:30:30 +0900

    Just trying it out

--- categories/views.py
+++ categories/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
+
+@api_view(["GET", "POST"])
+def categories(request):
+
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
 
 @api_view()
-def categories(request):
-    return Response(
-        {
-            'ok':True,
-            'categories':Category.objects.all(),
-        }
-    )
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
 
+
+# {
+# "name": "Category from DRF",
+# "kind": "rooms"
+# }
