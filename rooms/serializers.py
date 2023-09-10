7fd92cb93f263b494e1b33ad9972da7cad9c5e25
celestiarune <celestiarune@gmail.com>
2023-09-10 21:46:33 +0900

    Update serializers.py

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from .models import Room, Amenity
+
+
+class RoomListSerializer(ModelSerializer):
+
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+        )
+
+class RoomDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+        depth = 1
 
 class AmenitySerializer(ModelSerializer):
     
