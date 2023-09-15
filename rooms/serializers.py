135e7393abc861a9e69a2ba9f49535791c4c982d
celestiarune <celestiarune@gmail.com>
2023-09-15 17:43:43 +0900

    Update serializers.py

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Room, Amenity
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
@@ -15,6 +16,9 @@ class AmenitySerializer(ModelSerializer):
 
 class RoomListSerializer(ModelSerializer):
 
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
         fields = (
@@ -23,16 +27,36 @@ class RoomListSerializer(ModelSerializer):
             "country",
             "city",
             "price",
+            "rating",
+            "is_owner",
         )
+
+    def get_rating(self, room):
+        return room.rating()
+    
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
 
 class RoomDetailSerializer(ModelSerializer):
 
     owner = TinyUserSerializer(read_only=True)
     amenities = AmenitySerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+    
 
     class Meta:
         model = Room
         fields = "__all__"
+    
+    def get_rating(self, room):
+        return room.rating()
+    
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
 
+    
     
