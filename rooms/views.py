4c486063682eb15498a218c35ef5066d0402f263
celestiarune <celestiarune@gmail.com>
2023-09-21 22:39:58 +0900

    Update views.py

--- rooms/views.py
+++ rooms/views.py
@@ -8,6 +8,7 @@ from categories.models import Category
 from .serializers import RoomListSerializer, RoomDetailSerializer, AmenitySerializer
 from reviews.serializers import ReviewSerializer
 from django.conf import settings
+from medias.serializers import PhotoSerializer
 
 class Rooms(APIView):
     def get(self, request):
@@ -134,9 +135,26 @@ class RoomReviews(APIView):
 
 
 class RoomPhotos(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
 
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 
 
