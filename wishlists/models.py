4e31ebfd07d104d37af56731b8a61097e8b55f77
celestiarune <celestiarune@gmail.com>
2023-10-01 03:21:55 +0900

    update

--- wishlists/models.py
+++ wishlists/models.py
@@ -7,8 +7,8 @@ class Wishlist(CommonModel):
 
     """Wish List definition"""
     name = models.CharField(max_length=150)
-    rooms = models.ManyToManyField("rooms.Room", related_name="wishlists", null=True, blank=True)
-    experiences = models.ManyToManyField("experiences.Experience", related_name="wishlists", null=True, blank=True)
+    rooms = models.ManyToManyField("rooms.Room", related_name="wishlists")
+    experiences = models.ManyToManyField("experiences.Experience", related_name="wishlists")
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name="wishlists",)
 
     def __str__(self) -> str:
